602c6a4f743c792b2acb61209daf1a284fc9c7be
Randy <randyisbusy@gmail.com>
2020-02-01 19:05:10 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function addingEventListener() {
+
+function addingEventListener() { 
+
+    const input = document.getElementById('input');
+ 
+    input.addEventListener('click', function () {
+        alert('I was clicked!');
+    });
 }
+
